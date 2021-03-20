d50e911b06c2f4e57ecea8e96fc3c20966e3c7ab
dgponder <damon.g.ponder@nasa.gov>
2021-03-20 11:36:13 -0500

    first commit

---
+++ src/components/menu/menuLeft/menuLeft.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./menuLeft.Styles.scss";
+
+export default class MenuLeft extends Component {
+    render() {
+        let visibility = "hide";
+
+        if (this.props.menuVisibility) {
+            visibility = "show";
+        }
+
+        return (
+            <div id="flyoutMenuLeft"
+                 onMouseDown={this.props.handleMouseDown}
+                 className={visibility}>
+                <h2><a href="#">Recent Search Terms</a></h2>
+                <h2><a href="#">Trending Search Terms</a></h2>
+            </div>
+        );
+    }
+}
+
